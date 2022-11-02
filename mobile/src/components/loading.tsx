ced439d28e7f85d2a8a2767e6e92ed4d574cd904
Anthony Andrey <anthonyoliveira@chiptronic.com.br>
2022-11-02 14:54:26 -0300

    Add mobile project

---
+++ mobile/src/components/loading.tsx
@@ -0,0 +1,11 @@
+import {Center, Spinner} from 'native-base'
+
+
+
+export function Loading() {
+  return (
+   <Center flex={1} bg="gray.500">
+    <Spinner color="yellow.500"/>
+   </Center>
+  );
+}
---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,13 @@
+import { Center, Text } from 'native-base';
+
+
+export function Signin(){
+    return(
+        <Center flex={1} bgColor='gray.900' >
+      
+
+        <Text color="white" fontSize={24} fontFamily='heading'>
+          Open up App.tsx to start working on your app! Eu não</Text>
+      </Center >
+    )
+}
